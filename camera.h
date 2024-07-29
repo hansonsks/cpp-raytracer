c97664940def35e8d1abaf2a4f6439e921b6c3b6
hansonsks <hansonsks@gmail.com>
2024-07-29 15:52:29 +0100

    Updated main + camera files

--- camera.h
+++ camera.h
@@ -12,15 +12,16 @@
 
 class camera {
 public:
-    double aspect_ratio      = 1.0;  // Ratio of image width over height
-    int    img_width         = 100;  // Rendered image width in pixel count
-    int    samples_per_pixel = 10;   // Count of random samples for each pixel
-    int    max_depth         = 10;   // Maximum number of ray bounces into scene
+    double aspect_ratio      = 1.0;   // Ratio of image width over height
+    int    img_width         = 100;   // Rendered image width in pixel count
+    int    samples_per_pixel = 10;    // Count of random samples for each pixel
+    int    max_depth         = 10;    // Maximum number of ray bounces into scene
+    colour  background;               // Scene background colour
 
-    double vfov      = 90;                            // Vertical FOV
+    double vfov     = 90;                             // Vertical FOV
     point3 look_from = point3(0, 0, 0);   // Point camera is looking from
     point3 look_at   = point3(0, 0, -1);  // Point camera is looking at
-    vec3   vup       = vec3(0,1,0);       // Camera-relative "up" direction
+    vec3   vup      = vec3(0, 1, 0);      // Camera-relative "up" direction
 
     double defocus_angle = 0;  // Variation angle of rays through each pixel
     double focus_dist = 10;    // Distance from camera look_from point to plane of perfect focus
@@ -48,15 +49,15 @@ public:
     }
 
 private:
-    int    img_height{};            // Rendered image height
-    double pixel_samples_scale{};   // Colour scale factor for a sum of pixel samples
-    point3 center;                  // Camera center
-    point3 pixel00_loc;             // Location of pixel (0, 0)
-    vec3   pixel_delta_u;           // Offset to pixel to the right
-    vec3   pixel_delta_v;           // Offset to pixel below
-    vec3   u, v, w;                 // Camera frame basis vectors
-    vec3   defocus_disk_u;          // Defocus disk horizontal radius
-    vec3   defocus_disk_v;          // Defocus disk vertical radius
+    int    img_height;           // Rendered image height
+    double pixel_samples_scale;  // Colour scale factor for a sum of pixel samples
+    point3 center;               // Camera center
+    point3 pixel00_loc;          // Location of pixel 0, 0
+    vec3   pixel_delta_u;        // Offset to pixel to the right
+    vec3   pixel_delta_v;        // Offset to pixel below
+    vec3   u, v, w;              // Camera frame basis vectors
+    vec3   defocus_disk_u;       // Defocus disk horizontal radius
+    vec3   defocus_disk_v;       // Defocus disk vertical radius
 
     void initialize() {
         img_height = int(img_width / aspect_ratio);
@@ -68,11 +69,11 @@ private:
 
         // Determine viewport dimensions
         auto theta = degrees_to_radians(vfov);
-        auto h = std::tan(theta / 2);
+        auto h = std::tan(theta/2);
         auto viewport_height = 2 * h * focus_dist;
         auto viewport_width = viewport_height * (double(img_width) / img_height);
 
-        // Calculate the u,v,w unit basis vectors for the camera coordinate frame
+        // Calculate the u, v, w unit basis vectors for the camera coordinate frame
         w = unit_vector(look_from - look_at);
         u = unit_vector(cross(vup, w));
         v = cross(w, u);
@@ -89,7 +90,7 @@ private:
         auto viewport_upper_left = center - (focus_dist * w) - viewport_u / 2 - viewport_v / 2;
         pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
-        // Calculate the camera defocus disk basis vectors
+        // Calculate the camera defocus disk basis vectors.
         auto defocus_radius = focus_dist * std::tan(degrees_to_radians(defocus_angle / 2));
         defocus_disk_u = u * defocus_radius;
         defocus_disk_v = v * defocus_radius;
@@ -102,8 +103,9 @@ private:
 
         auto ray_origin = (defocus_angle <= 0) ? center : defocus_disk_sample();
         auto ray_direction = pixel_sample - ray_origin;
+        auto ray_time = random_double();
 
-        return {ray_origin, ray_direction};
+        return {ray_origin, ray_direction, ray_time};
     }
 
     // Returns the vector to a random point in the [-.5, -.5] to [+.5, +.5] unit square
@@ -130,18 +132,21 @@ private:
 
         hit_record rec;
 
-        if (world.hit(r, interval(0.001, infinity), rec)) {
-            ray scattered;
-            colour attenuation;
-            if (rec.mat->scatter(r, rec, attenuation, scattered)) {
-                return attenuation * ray_colour(scattered, depth-1, world);
-            }
-            return {0,0,0};
+        // If the ray hits nothing, return the background colour.
+        if (!world.hit(r, interval(0.001, infinity), rec)) {
+            return background;
         }
 
-        vec3 unit_direction = unit_vector(r.direction());
-        auto a = 0.5 * (unit_direction.y() + 1.0);
-        return (1.0 - a) * colour(1.0, 1.0, 1.0) + a * colour(0.5, 0.7, 1.0);
+        ray scattered;
+        colour attenuation;
+        colour colour_from_emission = rec.mat->emitted(rec.u, rec.v, rec.p);
+
+        if (!rec.mat->scatter(r, rec, attenuation, scattered)) {
+            return colour_from_emission;
+        }
+
+        colour colour_from_scatter = attenuation * ray_colour(scattered, depth - 1, world);
+        return colour_from_emission + colour_from_scatter;
     }
 };
 
